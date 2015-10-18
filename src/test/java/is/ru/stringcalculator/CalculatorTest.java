66b95cb989556aae6a64048ea433a723227bc9ff
Sveinn <sveinnt14@ru.is>
2015-10-18 14:29:00 +0000

    Stringcalc handles 0, 1, 2, 3 numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -15,8 +15,23 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void testOneNumber() {
+	public void testOneNumber1() {
 		assertEquals(1, Calculator.add("1"));
+	}
+
+	@Test
+	public void testOneNumber2() {
+		assertEquals(5, Calculator.add("5"));
+	}
+
+	@Test
+	public void testOneNumber3() {
+		assertEquals(9, Calculator.add("9"));
+	}
+
+	@Test
+	public void testOneNumber4() {
+		assertEquals(99, Calculator.add("99"));
 	}
 
 	@Test
@@ -28,5 +43,4 @@ public class CalculatorTest {
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
-
 }
