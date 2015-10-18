776f20943174f22fd47c97cc67aaf198d642d64f
Sveinn <sveinnt14@ru.is>
2015-10-18 22:34:40 +0000

    Stringcalc handles multiple delimiters

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,89 +1,122 @@
-package is.ru.stringcalculator;
+	package is.ru.stringcalculator;
 
-import java.util.Vector;
+	import java.util.Vector;
 
-public class Calculator {
+	public class Calculator {
 
-	public static int add(String text) {
-		if(text.startsWith("//")) {
-			String delim = deliminator(text);
-			text = text.substring(text.indexOf("\n") + 1);
+		public static int add(String text) {
+			if(text.startsWith("//")) {
+				String delim = deliminator(text);
 
-			delim = checkRegEx(delim);
+				text = text.substring(text.indexOf("\n") + 1);
 
-			return sum(splitNumbers(delim, text));
-		}
+				delim = checkRegEx(delim);
 
-		if(text.equals("")){
-			return 0;
-		}
-		else if(text.contains(",") || text.contains("\n")) {
-			String delim = ",|\n";
-			return sum(splitNumbers(delim, text));
-		}
-		else
-			return toInt(text);
-	}
-
-	private static int toInt(String number) {
-		return Integer.parseInt(number);
-	}
-
-	private static String[] splitNumbers(String delim, String numbers) {
-	    return numbers.split(delim);
-	}
-
-	private static String negativeError(Vector vec) {
-		String error = "";
-		if(vec.size() > 0) {
-			error = "Negatives not allowed: ";
-			error += vec.elementAt(0);
-			vec.remove(0);
-			
-			for(int i = 0; i < vec.size(); i++) {
-			error += ",";
-			error += vec.elementAt(i);
+				return sum(splitNumbers(delim, text));
 			}
+
+			if(text.equals("")){
+				return 0;
+			}
+			else if(text.contains(",") || text.contains("\n")) {
+				String delim = ",|\n";
+				return sum(splitNumbers(delim, text));
+			}
+			else
+				return toInt(text);
+		}
+
+		private static int toInt(String number) {
+			return Integer.parseInt(number);
+		}
+
+		private static String[] splitNumbers(String delim, String numbers) {
+		    return numbers.split(delim);
+		}
+
+		private static String negativeError(Vector vec) {
+			String error = "";
+			if(vec.size() > 0) {
+				error = "Negatives not allowed: ";
+				error += vec.elementAt(0);
+				vec.remove(0);
+				
+				for(int i = 0; i < vec.size(); i++) {
+				error += ",";
+				error += vec.elementAt(i);
+				}
+				return error;
+			}
+
 			return error;
 		}
 
-		return error;
-	}
+		private static String deliminator(String delim) {
+			String delimToReturn = "";
+			if(delim.charAt(2) == '[') {
+				String temp = "";
+				delimToReturn = delim.substring(2, delim.indexOf('\n'));
+				for(int i = 0; i < delimToReturn.length(); i++) {
+					if(delimToReturn.charAt(i) == '[') {
 
-	private static String deliminator(String delim) {
-		String tmp;
-		if(delim.charAt(2) == '[') {
-			return delim.substring(3, delim.indexOf("]"));
-		}
+						for(i = ++i; i < delimToReturn.length(); i++) {
+							if(delimToReturn.charAt(i) != ']'){
+							temp += delimToReturn.charAt(i);
+							} else {
+								temp += "|";
+								break;
+							}
+						}
+					}
+				}
+				delimToReturn = temp.substring(0, temp.length()-1);
 
-		return String.valueOf(delim.charAt(2));
-	}
-      
-    private static int sum(String[] numbers) {
-    	Vector<String> negatives = new Vector<String>();
- 	    int total = 0;
-        for(String number : numbers) {
-        	int tmp = toInt(number);
-        	if(tmp < 0) {
-        		negatives.add(number);
-        	} else {
-        		if(tmp > 1000) {
-        		tmp = 0;
-        		}
-
-        		total += tmp;
-        	}
-		}
-		    String error = negativeError(negatives);
-			if(error.length() > 0){
-			throw new RuntimeException(error);
+				return delimToReturn;
 			}
 
-		return total;
-    }
+			return String.valueOf(delim.charAt(2));
+		}
+	      
+	    private static int sum(String[] numbers) {
+	    	Vector<String> negatives = new Vector<String>();
+	 	    int total = 0;
+	        for(String number : numbers) {
+	        	int temp = toInt(number);
+	        	if(temp < 0) {
+	        		negatives.add(number);
+	        	} else {
+	        		if(temp > 1000) {
+	        		temp = 0;
+	        		}
 
-    private static String checkRegEx(String delim) {
-    		String tmp = ("\\Q" + delim + "\\E");
-    		return tmp;
-    }
-}
+	        		total += temp;
+	        	}
+			}
+			    String error = negativeError(negatives);
+				if(error.length() > 0){
+				throw new RuntimeException(error);
+				}
+
+			return total;
+	    }
+
+	    private static String checkRegEx(String delim) {
+	    	String tmp = "";
+	    	if(delim.contains("|")) {
+	    		for(int i = 0; i < delim.length(); i++) {
+	    			if(delim.charAt(i) != '|'){
+	    				tmp += "\\";
+	    				tmp += delim.charAt(i);
+	    			} else {
+	    				tmp += delim.charAt(i);
+	    			}
+	    			
+	    		}
+	    	}
+	    	else{
+	    		tmp = ("\\Q" + delim + "\\E");
+	    	}
+	    		
+	    		return tmp;
+	    }
+	}
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -1,145 +1,165 @@
-package is.ru.stringcalculator;
+    package is.ru.stringcalculator;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.rules.ExpectedException;
-import org.junit.Rule;
-import org.junit.Test;
+    import static org.junit.Assert.assertEquals;
+    import org.junit.rules.ExpectedException;
+    import org.junit.Rule;
+    import org.junit.Test;
 
-public class CalculatorTest {
+    public class CalculatorTest {
 
-	public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+    	public static void main(String args[]) {
+          org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+        }
+
+    	@Test
+    	public void testEmptyString() {
+    		assertEquals(0, Calculator.add(""));
+    	}
+
+    	@Test
+    	public void testOneNumber1() {
+    		assertEquals(1, Calculator.add("1"));
+    	}
+
+    	@Test
+    	public void testOneNumber2() {
+    		assertEquals(5, Calculator.add("5"));
+    	}
+
+    	@Test
+    	public void testOneNumber3() {
+    		assertEquals(9, Calculator.add("9"));
+    	}
+
+    	@Test
+    	public void testOneNumber4() {
+    		assertEquals(99, Calculator.add("99"));
+    	}
+
+    	@Test
+    	public void testTwoNumbers() {
+    		assertEquals(3, Calculator.add("1,2"));
+    	}	
+
+    	@Test
+        public void testMultipleNumbers() {
+        	assertEquals(6, Calculator.add("1,2,3"));
+        }
+
+        @Test
+        public void testMultipleNumbersNewDelim1() {
+        	assertEquals(6, Calculator.add("1\n2,3"));
+        }
+
+        @Test
+        public void testMultipleNumbersNewDelim2() {
+        	assertEquals(6, Calculator.add("1,2\n3"));
+        }
+
+        @Test
+        public void testMultipleNumbersNewDelim3() {
+        	assertEquals(10, Calculator.add("1\n2,3\n4"));
+        }
+
+        @Test
+        public void testMultipleNumbersNewDelim4() {
+        	assertEquals(6, Calculator.add("1\n2\n3"));
+        }
+
+        @Test
+        public void testDifferentDelim1() {
+        	assertEquals(3, Calculator.add("//;\n1;2"));
+        }
+
+        @Test
+        public void testDifferentDelim2() {
+        	assertEquals(6, Calculator.add("//:\n1:2:3"));
+        }
+
+        @Test
+        public void testDifferentDelim3() {
+        	assertEquals(6, Calculator.add("//*\n1*2*3"));
+        }
+
+        @Rule
+        public ExpectedException thrown = ExpectedException.none();
+
+        @Test
+        public void shouldTestExceptionMessage1() throws RuntimeException {
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Negatives not allowed: -4,-5");
+        Calculator.add("2,-4,3,-5");
+        }
+
+        @Test
+        public void shouldTestExceptionMessage2() throws RuntimeException {
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Negatives not allowed: -4,-5,-99");
+        Calculator.add("2,-4,3,-5,-99");
+        }
+
+        @Test
+        public void shouldTestExceptionMessage3() throws RuntimeException {
+
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Negatives not allowed: -5,-99,-75,-88");
+        Calculator.add("2,-5,-99,13,18,74,-75,-88");
+        }
+
+        @Test
+        public void ignoreNumbersLargerThan10001() {
+            assertEquals(2, Calculator.add("1001,2"));
+        }
+
+        @Test
+        public void ignoreNumbersLargerThan10002() {
+            assertEquals(1001, Calculator.add("1001,2,999"));
+        }
+
+        @Test
+        public void ignoreNumbersLargerThan10003() {
+            assertEquals(1001, Calculator.add("1001,1000,1"));
+        }
+
+        @Test
+        public void delimiterOfAnyLength() {
+            assertEquals(6, Calculator.add("//[***]\n1***2***3"));
+        }
+
+        @Test
+        public void delimiterOfAnyLength1() {
+            assertEquals(6, Calculator.add("//[***]\n1***2***3"));
+        }
+
+        @Test
+        public void delimiterOfAnyLength2() {
+            assertEquals(6, Calculator.add("//[{{{]\n1{{{2{{{3"));
+        }
+
+        @Test
+        public void delimiterOfAnyLength3() {
+            assertEquals(6, Calculator.add("//[,{,]\n1,{,2,{,3"));
+        }
+
+        @Test
+        public void multipleDelimiters1() {
+            assertEquals(6, Calculator.add("//[*][%]\n1*2%3"));
+        }
+
+        @Test
+        public void multipleDelimiters2() {
+            assertEquals(10, Calculator.add("//[*][{][%]\n1*2%3{4"));
+        }
+
+        @Test
+        public void multipleDelimiters3() {
+            assertEquals(10, Calculator.add("//[*{][{][%]\n1*{2%3{4"));
+        }
+
+        @Test
+        public void multipleDelimiters4() {
+            assertEquals(10, Calculator.add("//[*{][{*][*%]\n1*{2*%3{*4"));
+        }
     }
-
-	@Test
-	public void testEmptyString() {
-		assertEquals(0, Calculator.add(""));
-	}
-
-	@Test
-	public void testOneNumber1() {
-		assertEquals(1, Calculator.add("1"));
-	}
-
-	@Test
-	public void testOneNumber2() {
-		assertEquals(5, Calculator.add("5"));
-	}
-
-	@Test
-	public void testOneNumber3() {
-		assertEquals(9, Calculator.add("9"));
-	}
-
-	@Test
-	public void testOneNumber4() {
-		assertEquals(99, Calculator.add("99"));
-	}
-
-	@Test
-	public void testTwoNumbers() {
-		assertEquals(3, Calculator.add("1,2"));
-	}	
-
-	@Test
-    public void testMultipleNumbers() {
-    	assertEquals(6, Calculator.add("1,2,3"));
-    }
-
-    @Test
-    public void testMultipleNumbersNewDelim1() {
-    	assertEquals(6, Calculator.add("1\n2,3"));
-    }
-
-    @Test
-    public void testMultipleNumbersNewDelim2() {
-    	assertEquals(6, Calculator.add("1,2\n3"));
-    }
-
-    @Test
-    public void testMultipleNumbersNewDelim3() {
-    	assertEquals(10, Calculator.add("1\n2,3\n4"));
-    }
-
-    @Test
-    public void testMultipleNumbersNewDelim4() {
-    	assertEquals(6, Calculator.add("1\n2\n3"));
-    }
-
-    @Test
-    public void testDifferentDelim1() {
-    	assertEquals(3, Calculator.add("//;\n1;2"));
-    }
-
-    @Test
-    public void testDifferentDelim2() {
-    	assertEquals(6, Calculator.add("//:\n1:2:3"));
-    }
-
-    @Test
-    public void testDifferentDelim3() {
-    	assertEquals(6, Calculator.add("//*\n1*2*3"));
-    }
-
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Test
-    public void shouldTestExceptionMessage1() throws RuntimeException {
-
-    thrown.expect(RuntimeException.class);
-    thrown.expectMessage("Negatives not allowed: -4,-5");
-    Calculator.add("2,-4,3,-5");
-    }
-
-    @Test
-    public void shouldTestExceptionMessage2() throws RuntimeException {
-
-    thrown.expect(RuntimeException.class);
-    thrown.expectMessage("Negatives not allowed: -4,-5,-99");
-    Calculator.add("2,-4,3,-5,-99");
-    }
-
-    @Test
-    public void shouldTestExceptionMessage3() throws RuntimeException {
-
-    thrown.expect(RuntimeException.class);
-    thrown.expectMessage("Negatives not allowed: -5,-99,-75,-88");
-    Calculator.add("2,-5,-99,13,18,74,-75,-88");
-    }
-
-    @Test
-    public void ignoreNumbersLargerThan10001() {
-        assertEquals(2, Calculator.add("1001,2"));
-    }
-
-    @Test
-    public void ignoreNumbersLargerThan10002() {
-        assertEquals(1001, Calculator.add("1001,2,999"));
-    }
-
-    @Test
-    public void ignoreNumbersLargerThan10003() {
-        assertEquals(1001, Calculator.add("1001,1000,1"));
-    }
-
-    @Test
-    public void delimiterOfAnyLength() {
-        assertEquals(6, Calculator.add("//[***]\n1***2***3"));
-    }
-
-    @Test
-    public void delimiterOfAnyLength1() {
-        assertEquals(6, Calculator.add("//[***]\n1***2***3"));
-    }
-
-    @Test
-    public void delimiterOfAnyLength2() {
-        assertEquals(6, Calculator.add("//[{{{]\n1{{{2{{{3"));
-    }
-
-    @Test
-    public void delimiterOfAnyLength3() {
-        assertEquals(6, Calculator.add("//[,{,]\n1,{,2,{,3"));
-    }
-}
